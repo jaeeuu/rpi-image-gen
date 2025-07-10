02baf2ace5111dc7d1c517dc6213e1fb9d2ffc6e
jaeeuu <jreset@naver.com>
2025-07-10 17:10:46 +0900

    revert to mbr

--- autokiosk/device/custom-device/post-build.sh
+++ autokiosk/device/custom-device/post-build.sh
@@ -5,10 +5,10 @@ set -eu
 # Mask unwanted services to prevent power-saving actions or delays (if present)
 ln -sf /dev/null $1/etc/systemd/system/systemd-suspend.service 2>/dev/null || true
 ln -sf /dev/null $1/etc/systemd/system/systemd-hibernate.service 2>/dev/null || true
-ln -sf /dev/null $1/etc/systemd/system/apt-daily.service
-ln -sf /dev/null $1/etc/systemd/system/apt-daily.timer
-ln -sf /dev/null $1/etc/systemd/system/apt-daily-upgrade.service
-ln -sf /dev/null $1/etc/systemd/system/apt-daily-upgrade.timer
+ln -sf /dev/null $1/etc/systemd/system/apt-daily.service 2>/dev/null || true
+ln -sf /dev/null $1/etc/systemd/system/apt-daily.timer 2>/dev/null || true
+ln -sf /dev/null $1/etc/systemd/system/apt-daily-upgrade.service 2>/dev/null || true
+ln -sf /dev/null $1/etc/systemd/system/apt-daily-upgrade.timer 2>/dev/null || true
 # rootfs="$1"
 
 # ------ disable display blanking ---------------------------
