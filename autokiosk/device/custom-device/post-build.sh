99bc82b45c5afa6b21dfae34ebe01643a273ebeb
jaeeuu <jreset@naver.com>
2025-07-10 23:01:39 +0900

    test

--- autokiosk/device/custom-device/post-build.sh
+++ autokiosk/device/custom-device/post-build.sh
@@ -2,25 +2,18 @@
 
 set -eu
 
-# Mask unwanted services to prevent power-saving actions or delays (if present)
+rootfs="$1"
+
 ln -sf /dev/null $1/etc/systemd/system/systemd-suspend.service 2>/dev/null || true
 ln -sf /dev/null $1/etc/systemd/system/systemd-hibernate.service 2>/dev/null || true
+ln -sf /dev/null $1/etc/systemd/system/getty-static.service 2>/dev/null || true
 ln -sf /dev/null $1/etc/systemd/system/apt-daily.service 2>/dev/null || true
 ln -sf /dev/null $1/etc/systemd/system/apt-daily.timer 2>/dev/null || true
 ln -sf /dev/null $1/etc/systemd/system/apt-daily-upgrade.service 2>/dev/null || true
 ln -sf /dev/null $1/etc/systemd/system/apt-daily-upgrade.timer 2>/dev/null || true
-# rootfs="$1"
-
-# ------ disable display blanking ---------------------------
-
-#echo 'consoleblank=0' >> "${BOOTFS_DIR}/cmdline.txt"
-
-# ------ EEPROM tweaks --------------------------------------
-# cat >> "$1/etc/rc.local" <<'EOF'
-# /usr/bin/rpi-eeprom-config --edit <<EOC
-# [all]
-# WAIT_FOR_POWER_BUTTON=0
-# POWER_OFF_ON_HALT=0
-# EOC
-# exit 0    # keep rc.local happy
-# EOF
+if [ -f "$1/etc/systemd/logind.conf" ]; then
+  sed -i \
+    -e 's/^[[:space:]]*#\?[[:space:]]*NAutoVTs=.*/NAutoVTs=0/' \
+    -e 's/^[[:space:]]*#\?[[:space:]]*ReserveVT=.*/ReserveVT=0/' \
+    "$1/etc/systemd/logind.conf"
+fi
